3861f19aecd0951617aae8bd3c4811a619029dc7
sabgilhun <sabgilhun@gmail.com>
2020-10-01 02:25:08 +0900

    modify sample code

--- app/src/main/java/com/sabgil/extraofsample/MainActivity.kt
+++ app/src/main/java/com/sabgil/extraofsample/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.sabgil.extraofsample
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sabgil.annotation.Extra
@@ -8,8 +10,13 @@ import com.sabgil.extraof.extraOf
 class MainActivity : AppCompatActivity() {
 
     @Extra
-    private val aqsASASAS: String by extraOf()
+    private val aqsASASAS: Int by extraOf()
 
+    companion object {
+        fun start(context: Context) {
+            context.startActivity(Intent(context, MainActivity::class.java).apply { putExtra("aqsASASAS", 259) })
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/sabgil/extraofsample/MainActivity2.kt
+++ app/src/main/java/com/sabgil/extraofsample/MainActivity2.kt
@@ -1,17 +1,17 @@
 package com.sabgil.extraofsample
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.sabgil.annotation.Extra
-import com.sabgil.extraof.extraOf
 
 class MainActivity2 : AppCompatActivity() {
-
-    @Extra
-    private val a: Int by extraOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.go).setOnClickListener {
+            MainActivity.start(this)
+        }
     }
 }
